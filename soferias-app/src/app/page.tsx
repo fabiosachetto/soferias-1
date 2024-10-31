c4ea439e768a6dec1e9dbd3c41a78cfed7490b91
fabiosachetto <fabio@sachetto.com.br>
2024-10-30 22:42:44 -0300

    adicionando selects

--- soferias-app/src/app/page.tsx
+++ soferias-app/src/app/page.tsx
@@ -34,7 +34,20 @@ export default function Home() {
             <div>
               <div className="flex items-center justify-between">
                 <label htmlFor="date" className="block text-sm/6 font-medium text-gray-900">
-                  Dia Procurado
+                  Check-in
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="date"
+                  name="date"
+                  type="date"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+                />
+              </div>
+              <div className="flex mt-3 items-center justify-between">
+                <label htmlFor="date" className="block text-sm/6 font-medium text-gray-900">
+                  Check-out
                 </label>
               </div>
               <div className="mt-2">
