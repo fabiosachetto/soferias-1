09839a8ab1511faa3b250d419be716463681db18
fabiosachetto <fabio@sachetto.com.br>
2024-10-31 17:13:02 -0300

    subindo imagens e retirando imagem tosca da tela nossa casa

--- soferias-app/src/app/nossacasa/page.tsx
+++ soferias-app/src/app/nossacasa/page.tsx
@@ -1,3 +1,4 @@
+import Entrada from "@/app/images/entrada.jpg";
 import Image from "next/image";
 
 export default function Planos() {
@@ -6,10 +7,10 @@ export default function Planos() {
       <h1 className="text-9xl text-white">Nossa Casa</h1>
 
       <Image
-        src="https://preview.redd.it/fumava-o-fino-ou-bolava-um-vela-kk-flor-com-quase-5-meses-v0-6m1xrjbd04xd1.jpeg?width=640&crop=smart&auto=webp&s=0c97ecb622984fee5338c05ab6c94360be9e3fc3"
-        alt="Maconha"
-        width={200}
-        height={200}
+        src={Entrada}
+        alt="Fachada"
+        width={680}
+        height={510}
       />
     </div>
   );
