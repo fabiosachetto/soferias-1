8f1d90aee2a9c7fc849d7bfe8182fe0f4c84fcfd
fabiosachetto <fabio@sachetto.com.br>
2024-10-31 22:01:28 -0300

    arrumando cagada e criando image slider

--- soferias-app/src/app/suites/page.tsx
+++ soferias-app/src/app/suites/page.tsx
@@ -1,5 +1,18 @@
+import ImageSlider from '@/components/ImageSlider';
+
 export default function Suites() {
   return (
-    <h1 className="text-9xl text-white">Suítes.</h1>
+
+
+    <div id="default-carousel" className="relative w-full" data-carousel="slide">
+
+      <h1 className="text-9xl text-white">Suítes.</h1>
+      <div>
+        <main className="flex min-h-screen flex-col items-center justify-center py-2">
+          <ImageSlider />
+        </main>
+      </div>
+    </div>
+
   );
 };
