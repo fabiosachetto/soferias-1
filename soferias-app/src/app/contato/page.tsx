f75d7c7e6088375f09b6c2128c38e3e334742695
fabiosachetto <fabio@sachetto.com.br>
2024-11-10 21:20:14 -0300

    criando formulario de contato

--- soferias-app/src/app/contato/page.tsx
+++ soferias-app/src/app/contato/page.tsx
@@ -2,6 +2,71 @@
 
 export default function Contato() {
   return (
-    <h1 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Contato</h1>
+    <div className="flex min-h-full justify-center px-6 lg:px-8">
+
+
+      <div className="rounded-lg bg-white bg-opacity-70 w-1/2">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            Preencha o Formulário abaixo que entraremos em contato.
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="name" className="block text-sm/6 font-medium text-gray-900">
+                  Nome
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="name"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                E-mail
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="message" className="block text-sm/6 font-medium text-gray-900">
+                Mensagem
+              </label>
+              <textarea id="message" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Escreva aqui..."></textarea>
+            </div>
+
+
+            <div className="pb-12">
+              <button
+                type="submit"
+                className="flex w-full justify-center px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 rounded-lg cursor-pointer border-blue-800 hover:border-blue-500 bg-blue-500 hover:bg-blue-800"
+              >
+                Enviar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
