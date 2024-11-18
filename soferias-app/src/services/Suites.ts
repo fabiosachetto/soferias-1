bd947cbb00a122c4d0edb3eb7a49fede1a35a75f
fabiosachetto <fabio@sachetto.com.br>
2024-11-18 04:40:50 -0300

    trocando imagens e iniciando camada de serviços

---
+++ soferias-app/src/services/Suites.ts
@@ -0,0 +1,11 @@
+// import Suite from "@/libs/database/Suites";
+
+// const SuiteService = {
+//   getSuites : async (page=1, limit=10) => {
+
+//     Suite.get({})
+
+//   }
+// };
+
+// export default SuiteService;
